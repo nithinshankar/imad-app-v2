1ddc1c590fd2a759abbac9db6a374d8fe1aa97ee
nithinshankar <nithin.goku07@gmail.com>
2017-02-07 20:49:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,14 @@ console.log('Loaded!');
 var ele = document.getElementById('mtext');
 
 ele.innerHTML = 'MASS DAW';
+
+var ml=0;
+var img = document.getElementById('img');
+function moveRight(){
+    ml=ml+1;
+    img.style.ml=ml+'px';
+}
+
+img.onclick = function() {
+    var interval = setInterval(moveRight,50);
+};
